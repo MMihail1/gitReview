9e86ab8a46ee2912bd7880c6c95d784e26d2f934
MMikhail1 <aprrp32@gmail.com>
2022-06-01 07:54:30 +0600

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -1,10 +1,8 @@
 package gitReview;
-
 public class gitReview01 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("New example to put it on github");
+		System.out.println("Make some change");
+		System.out.println("Enjoy JAVA");
 	}
-
 }
